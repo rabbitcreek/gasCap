d837910879f1859ec90ba41cc6fd97a00bc2eb6a
rabbitcreek <rabbitcreek@gmail.com>
2025-09-03 10:36:52 -0800

    Add files via upload
    this is  the initial software for using the lps28 got checking pressure for gas cap ... it sounds small buzzer when difference between original pressure(atm) and tube pressure exceeds  2 mb....and increases rate of bleeping as pressure gets higher

---
+++ gasCap.ino
@@ -0,0 +1,75 @@
+#include <Wire.h>
+#include <Adafruit_LPS28.h>
+float pressure = 0;
+float diff = 0.0;
+// Instantiate the sensor
+Adafruit_LPS28 lps28;
+
+void setup() {
+  Serial.begin(115200);
+  //while (!Serial) delay(10); // Wait for Serial monitor
+
+  Serial.println("LPS28 Test");
+pinMode(D9, OUTPUT);
+digitalWrite(D9, LOW);
+  // Initialize the sensor
+  if (!lps28.begin()) {
+    Serial.println("Failed to find LPS28 sensor!");
+    while (1) delay(10);
+  }
+
+  Serial.println("LPS28 sensor found!");
+
+  // Set the highest ODR (200 Hz) and 4-sample averaging, new value every 20ms
+  lps28.setDataRate(LPS28_ODR_200_HZ);
+  lps28.setAveraging(LPS28_AVG_4);
+
+  // Set range to 4060 hPa
+  lps28.setFullScaleMode(true);
+
+  // Enable DRDY interrupt on the interrupt pin
+  lps28.setInterruptPin(
+    true,  // Polarity: Active high
+    false  // Pin mode: Push-pull
+  );
+
+  // Enable DRDY interrupt output on INT pin (we could use this with an interrupt)
+  lps28.setIntPinOutput(
+    true,  // DRDY active
+    false, // DRDY pulse not enabled
+    false, // INT output not enabled
+    false, // FIFO full interrupt not enabled
+    false, // FIFO watermark interrupt not enabled
+    false  // FIFO overrun interrupt not enabled
+  );
+   if (lps28.getStatus() & LPS28_STATUS_PRESS_READY) { // Pressure data available
+    // Read and print pressure and temperature
+     pressure = lps28.getPressure();
+   }
+}
+
+void loop() {
+  // Check if data is ready by reading the STATUS register
+  if (lps28.getStatus() & LPS28_STATUS_PRESS_READY) { // Pressure data available
+    // Read and print pressure and temperature
+    float pressureNow = lps28.getPressure();
+    float temperature = lps28.getTemperature();
+
+    Serial.print("Pressure (hPa): ");
+    Serial.println(pressure);
+
+    Serial.print("Temperature (°C): ");
+    Serial.println(temperature);
+    diff = pressureNow -  pressure;
+    if(diff > 2) ringDing();
+  }
+
+  delay(10); // Polling delay
+}
+void ringDing(){
+digitalWrite(D9, HIGH);
+delay(1000/ diff);
+digitalWrite(D9,LOW);
+delay(1000/ diff);
+
+}
